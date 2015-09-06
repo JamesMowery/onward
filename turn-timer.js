41582ef2589da56f5682fe66eaa7687f3c7de490
James Mowery <jmowery@gmail.com>
2015-09-06 16:08:51 -0400

    Sets up class and ready functions

--- turn-timer.js
+++ turn-timer.js
@@ -1,3 +1,13 @@
 // GitHub:    https://github.com/JamesMowery/turn-timer
 // By:        James Mowery
 // Contact:   https://app.roll20.net/users/1001679/james-mowery
+
+var TurnTimer = TurnTimer || (function() {
+    'use strict';
+
+    var version = "0.0.1";
+});
+
+on("ready", function() {
+    log("Starting Up");
+});
